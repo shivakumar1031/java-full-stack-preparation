43f5c2265e214c765e0aac511f7b7e8aca862bc5
shivakumar1031 <shivakumarchamanthula@gmail.com>
2026-08-21 10:34:57 +0530

    problem of the day 21-08-26

---
+++ 08-LeetCode/0014-LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder str= new StringBuilder();
+        boolean flag=false,comChar=false;
+        for(int i=0;i<strs[0].length();i++){
+            char ch=strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i>strs[j].length()-1 || ch!=strs[j].charAt(i))
+                    return str.toString();   
+            }
+            str.append(ch);
+        }
+        return str.toString();
+    }
+}
