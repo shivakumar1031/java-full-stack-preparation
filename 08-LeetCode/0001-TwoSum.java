85c52ce73db80d041d8ad377d9aae29f5320d893
shivakumar1031 <shivakumarchamanthula@gmail.com>
2026-08-25 16:02:59 +0530

    problem of the day 25-08-26

---
+++ 08-LeetCode/0001-TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        Map<Integer,Integer> map=new HashMap();
+        for(int i=0;i<nums.length;i++){
+            int compare=target-nums[i];
+            if(map.containsKey(compare)){
+                return new int[]{map.get(compare),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
