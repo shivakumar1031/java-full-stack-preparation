43a04cffa8f9c6a38b1782a693bcfe527aa30875
shivakumar1031 <shivakumarchamanthula@gmail.com>
2026-08-30 00:13:51 +0530

    problem of the day 30-08-26

--- 08-LeetCode/0347-TopKFrequentElements.java
+++ 08-LeetCode/0347-TopKFrequentElements.java
@@ -5,11 +5,11 @@ class Solution {
         for(int i: nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        int[] arr= new int[k];
-        List<Entry<Integer,Integer>> list = new ArrayList<>(map.EntrySet());
-        list.sort((a,b)->a-b);
-        for(int i=0;i<=k;i++){
-            arr[i]=list.get(i);
+        
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a,b)->b.getValue()-a.getValue());
+        for(int i=0;i<k;i++){
+            arr[i]=list.get(i).getKey();
         }
         return arr;
     }
